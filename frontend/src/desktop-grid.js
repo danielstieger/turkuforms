8a381106f2e5b56d6ae439e1db773bd8931857ff
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2023-11-07 13:56:55 +0100

    Editing with multi edit pattern, solution 1

--- frontend/src/desktop-grid.js
+++ frontend/src/desktop-grid.js
@@ -12,20 +12,29 @@ window.modellwerkstatt_desktopgrid = {
 
     focusGrid : function(grid, reschedule) {
         setTimeout(function() {
-          let editDiv = grid.shadowRoot.querySelector("[aria-selected='true'] > div[part~='editable-cell']");
-          if (editDiv) {
-              editDiv.parentElement.focus();
+
+          let openPromptWindow = document.querySelector('vaadin-dialog-overlay');
+          if (openPromptWindow) {
+             // do not handle focus, we are in background
 
           } else {
-              let firstTd = grid.shadowRoot.querySelector('[aria-selected="true"] > td');
-              if (firstTd) {
-                  firstTd.focus();
+            let editDiv = grid.shadowRoot.querySelector("[aria-selected='true'] > div[part~='editable-cell']");
+            if (editDiv) {
+                editDiv.parentElement.focus();
 
-              } else if (reschedule) {
-                  setTimeout(() => { modellwerkstatt_desktopgrid.focusGrid(grid, false); }, 1500);
-              }
+            } else {
+                let firstTd = grid.shadowRoot.querySelector('[aria-selected="true"] > td');
+                if (firstTd) {
+                    firstTd.focus();
 
-          }
-        }, 500);
+                } else if (reschedule) {
+                    setTimeout(() => { modellwerkstatt_desktopgrid.focusGrid(grid, false); }, 1500);
+                }
+
+            }
+
+         }
+
+        }, 300);
     },
 }
