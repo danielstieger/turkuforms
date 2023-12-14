461c1fbcf8a19a9329d2ebb3e82b03ca20dde646
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2023-12-14 15:24:03 +0100

    Simpler Solution to RadioDisabled visualization

--- src/main/java/org/modellwerkstatt/turkuforms/editors/RadioStatusEditor.java
+++ src/main/java/org/modellwerkstatt/turkuforms/editors/RadioStatusEditor.java
@@ -42,25 +42,11 @@ public class RadioStatusEditor extends EditorBasis<RadioButtonGroup<String>> imp
         return items.indexOf(cachedValue);
     }
 
-    protected void adjustEnabled() {
-        /* if (! cachedEnabledState) {
-            inputField.setItems(cachedValue);
-            inputField.setValue(cachedValue);
-        }*/
-    }
-
     @Override
     public void setSelectedIndex(int i) {
         cachedValue = items.get(i);
         lastIssuedUpdateText = cachedValue;
         inputField.setValue(cachedValue);
-        adjustEnabled();
-    }
-
-    @Override
-    public void setEnabled(boolean b) {
-        super.setEnabled(b);
-        adjustEnabled();
     }
 
     @Override
