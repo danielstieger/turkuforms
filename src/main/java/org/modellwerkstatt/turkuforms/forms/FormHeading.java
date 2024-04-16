616dac782b6c22663858d3a13683c6d76680fd71
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2024-04-16 14:54:42 +0200

    Form Heading refactored

--- src/main/java/org/modellwerkstatt/turkuforms/forms/FormHeading.java
+++ src/main/java/org/modellwerkstatt/turkuforms/forms/FormHeading.java
@@ -2,43 +2,63 @@ package org.modellwerkstatt.turkuforms.forms;
 
 import com.vaadin.flow.component.html.Div;
 import org.modellwerkstatt.objectflow.runtime.IOFXProblem;
+import org.modellwerkstatt.objectflow.runtime.Workarounds2;
 
 import java.util.List;
 
 public class FormHeading extends Div {
-    private Div heading;
+    private Div titleDiv;
+    private Div subTitleDiv;
+
+    private String[] splittedText = new String[]{"", null};
 
     public FormHeading() {
         this.addClassName("FormHeading");
-        heading = new Div();
-        this.add(heading);
+        titleDiv = new Div();
+        titleDiv.addClassName("FormHeadingTitle");
+
+        this.add(titleDiv);
     }
 
     public void inRootPos() {
-        heading.getElement().setAttribute("pageTitle", "true");
+        this.getElement().setAttribute("pageTitle", "true");
     }
 
     public void setHeading(String label) {
-        heading.setText(label);
+        splittedText = Workarounds2.getMainAndSubTitle(label);
+        titleDiv.setText(splittedText[0]);
+
+        if (splittedText[1] != null) {
+            if (subTitleDiv == null) {
+                subTitleDiv = new Div();
+                subTitleDiv.addClassName("FormHeadingSubTitle");
+                this.add(subTitleDiv);
+            }
+            subTitleDiv.setText(splittedText[1]);
+
+        } else if (subTitleDiv != null) {
+            this.remove(subTitleDiv);
+            subTitleDiv = null;
+
+        }
     }
 
     public void flag(List<IOFXProblem> problemList) {
-        boolean hasHeading = heading != null;
 
         this.removeAll();
 
-        if (hasHeading) {
-            this.add(heading);
+        if (titleDiv != null) {
+            this.add(titleDiv);
+        }
+
+        if (subTitleDiv != null) {
+            this.add(subTitleDiv);
         }
 
         for (IOFXProblem prblm: problemList) {
-            Div info =new Div();
+            Div info = new Div();
             info.setText(prblm.getSimpleUserText());
-            if (prblm.isWarningOnly()) {
-                info.addClassName("TurkuWarningDiv");
-            }else{
-                info.addClassName("TurkuErrorDiv");
-            }
+            info.addClassName(prblm.isWarningOnly() ? "TurkuWarningDiv" : "TurkuErrorDiv");
 
             this.add(info);
         }
