14f9628c011a3928dd85ffdfd71dc6295a9be3f8
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2024-09-23 11:40:54 +0200

    Working on google auth ...

--- src/main/java/org/modellwerkstatt/turkuforms/authmpreis/AuthDemo.java
+++ src/main/java/org/modellwerkstatt/turkuforms/authmpreis/AuthDemo.java
@@ -2,7 +2,9 @@ package org.modellwerkstatt.turkuforms.authmpreis;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.googleapis.testing.auth.oauth2.MockGoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -12,7 +14,6 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinSession;
 import org.modellwerkstatt.turkuforms.util.Turku;
 
@@ -31,14 +32,22 @@ public class AuthDemo extends HorizontalLayout implements BeforeEnterObserver {
 
 
         Turku.l("AuthDemo.beforeEnter()  on session " + VaadinSession.getCurrent().hashCode());
-
+        VaadinSession session = VaadinSession.getCurrent();
         List<String> codes = beforeEnterEvent.getLocation().getQueryParameters().getParameters().get("code");
         List<String> states = beforeEnterEvent.getLocation().getQueryParameters().getParameters().get("state");
-        Turku.l("                        codes " + (codes));
+        String theState = null;
+        String theCode = null;
+        String errorMessage = "";
+        String originalCode = "" + session.hashCode();
 
         if (codes != null && states != null) {
-            Turku.l("                      " + codes.get(0) + " / " + states.get(0));
+            theState = states.get(0);
+            theCode = codes.get(0);
+        }
 
+
+
+        if (theCode != null && originalCode.equals(theState)) {
             HttpTransport netTransport = new NetHttpTransport();
             JsonFactory jsonFactory = new GsonFactory();
             GoogleTokenResponse token;
@@ -49,32 +58,36 @@ public class AuthDemo extends HorizontalLayout implements BeforeEnterObserver {
                         jsonFactory,
                         "938810109726-3lo3a5ebkq8u8lqli5prjq3609bkkn6h.apps.googleusercontent.com",
                         "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",
-                        codes.get(0),
+                        theCode,
                         "http://localhost:8080/simpleone/login").execute();
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            Turku.l("The token is " + token);
-
-            try {
                 add(new Label(token.toPrettyString()));
 
+
+
+
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                errorMessage = "IoException - " + e.getMessage() + ". ";
+
             }
 
-        } else {
 
-            add(new SimpleMessageCmpt("TEST", "Go", "authenticate please", () -> {
 
-                // logout ?
+        } else if (theCode != null) {
+            // code does not fit ..
+            errorMessage = "Problems with the state while checking details with google. ";
+
+        }
+
+
+        if (theCode == null || !"".equals(errorMessage)) {
+
+            add(new SimpleMessageCmpt("Google Authentication", "Go", errorMessage + "Authenticated with google.", () -> {
 
                 String url = new GoogleBrowserClientRequestUrl("938810109726-3lo3a5ebkq8u8lqli5prjq3609bkkn6h.apps.googleusercontent.com",
                         "http://localhost:8080/simpleone/login", Arrays.asList(
                         "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"))
-                        .setState("/profile").setResponseTypes(Arrays.asList("code")).build();
-
+                        .setState(originalCode).setResponseTypes(Arrays.asList("code")).build();
 
                 UI.getCurrent().getPage().setLocation(url);
 
