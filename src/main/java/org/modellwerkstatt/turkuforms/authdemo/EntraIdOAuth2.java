868775fd1ef14837423438d6de6d59b7b379aaee
Daniel Stieger <daniel.stieger@modellwerkstatt.org>
2024-10-30 15:06:10 +0100

    Adjusted EntraIdOAuth2 handler with query specification

--- src/main/java/org/modellwerkstatt/turkuforms/authdemo/EntraIdOAuth2.java
+++ src/main/java/org/modellwerkstatt/turkuforms/authdemo/EntraIdOAuth2.java
@@ -11,24 +11,28 @@ import java.util.HashMap;
 import static org.modellwerkstatt.turkuforms.authdemo.GoogleOAuth2.httpConnection;
 
 public class EntraIdOAuth2 implements ExtAuthProvider {
-    public String AUTHINIT_ENDPOINT ="https://login.microsoftonline.com/e68f2ef1-5ffd-426f-b5d2-cec606c8a21a/oauth2/v2.0/authorize";
-    public String TOKEN_ENDPOINT ="https://login.microsoftonline.com/e68f2ef1-5ffd-426f-b5d2-cec606c8a21a/oauth2/v2.0/token";
-    public String USERINFO_ENDPOINT="https://graph.microsoft.com/v1.0/me";
-    public String SCOPE="User.Read";
+    private String AUTHINIT_ENDPOINT ="https://login.microsoftonline.com/-TENANT-/oauth2/v2.0/authorize";
+    private String TOKEN_ENDPOINT ="https://login.microsoftonline.com/-TENANT-/oauth2/v2.0/token";
+    private String SCOPE="User.Read";
 
 
     private String CLIENT_ID="not set";
     private String REDIRECT_URI="not set";
     private String CLIENT_SECRET="not set";
     private String USERNAME_FIELD_TO_USE="not set";
+    public String USERINFO_ENDPOINT="https://graph.microsoft.com/v1.0/me";
 
 
 
 
-    public EntraIdOAuth2(String client_id, String client_secret, String redirect_to, String username_field)  {
+
+    public EntraIdOAuth2(String tenant, String client_id, String client_secret, String redirect_to, String query_url, String username_field)  {
+        AUTHINIT_ENDPOINT = AUTHINIT_ENDPOINT.replace("-TENANT-", tenant);
+        TOKEN_ENDPOINT = TOKEN_ENDPOINT.replace("-TENANT-", tenant);
         CLIENT_ID = client_id;
         CLIENT_SECRET = client_secret;
         REDIRECT_URI =redirect_to;
+        USERINFO_ENDPOINT = query_url;
         USERNAME_FIELD_TO_USE = username_field;
     }
 
@@ -64,6 +68,7 @@ public class EntraIdOAuth2 implements ExtAuthProvider {
         object = Json.parse(content);
         if (!object.hasKey(USERNAME_FIELD_TO_USE)) { throw new RuntimeException("The field " + USERNAME_FIELD_TO_USE + " was not found in oauth2 servers return." + content); }
 
+        System.err.println("EntraIdOAuth2 " + object.toString());
         content = object.get(USERNAME_FIELD_TO_USE).asString();
 
         return content;
